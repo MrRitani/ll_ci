21a1929dc46649ad654af534c135c6eb38a47c0e
MrRitani <mrrit@hotmail.com>
2018-03-25 17:09:09 +0200

    Basic Header

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,23 @@
-ReactDOM.render( 
-	<h1>Test Launch</h1>, 
+const Title = (props) => {
+	const TitleTag = props.titleTag;
+
+	return <TitleTag>{props.children}</TitleTag>
+
+};
+
+const Header = (props) => (
+	<header>
+		<Title titleTag = {props.titleTag}>{props.titleName}</Title>
+	</header>
+);
+
+
+ReactDOM.render(
+	<div>
+		<Header
+			titleTag = 'h1'
+			titleName = 'React Example Page'
+		/>
+	</div>,
 	document.getElementById('app')
 );
